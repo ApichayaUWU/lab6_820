986684feb132f8fbbe9584238bd28fb69b0235eb
apichaya <apichaya_ink@cmu.ac.th>
2022-12-09 15:35:23 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,35 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int num,even=0,odd=0;
+    cout << "Enter an integer: " ;
+    cin >> num;
+    if(num%2 == 0){
+        if(num!=0){
+            even=even+1;
+        }else{
+            even = even;
+            }
+    }else{
+        odd=odd+1;
+    }
+    while (num != 0)
+    {
+        cout << "Enter an integer: " ;
+        cin >> num;
+          if(num%2 == 0){
+             if(num!=0){
+            even=even+1;
+            }else{
+            even = even;
+            }
+          }
+          else{
+             odd=odd+1;
+          }
+    
+    }
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = " << odd << endl;
     return 0;
 }
